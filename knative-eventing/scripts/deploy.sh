92940c6ba0935c98924dd492467d05b7b36057db
Juan Páez <jdpluc302@gmail.com>
2025-10-15 23:13:13 -0500

    feat: update scripts and correct kafka uri

--- knative-eventing/scripts/deploy.sh
+++ knative-eventing/scripts/deploy.sh
@@ -8,36 +8,40 @@ echo "======================================"
 # Create namespace
 echo "📦 Creating namespace..."
 kubectl create namespace medisupply-eventing --dry-run=client -o yaml | kubectl apply -f -
-#kubectl create namespace procurement-supply-optimization
+kubectl create namespace procurement-supply-optimization --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace commerce-sales --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace financial-billing --dry-run=client -o yaml | kubectl apply -f -
 # Deploy broker
 echo "🔧 Creating event broker..."
 kubectl apply -f  broker.yaml
+kubectl apply -f triggers/purchase-trigger.yaml
+kubectl apply -f triggers/sales-trigger.yaml
 
 # Wait for broker to be ready
 echo "⏳ Waiting for broker to be ready..."
-kubectl wait --for=condition=Ready --timeout=60s broker/medisupply-broker -n medisupply-eventing
+kubectl wait --for=condition=Ready --timeout=60s broker/medisupply-broker
 
 # Deploy services
 echo "🛠️  Deploying services..."
 
 # Procurement Supply Optimization
-kubectl apply -f ../procurement-supply-optimization/kafka.yaml
-kubectl apply -f ../procurement-supply-optimization/supplier.yaml
-kubectl apply -f ../procurement-supply-optimization/purchase-plans.yaml
-kubectl apply -f ../procurement-supply-optimization/contracts.yaml
+kubectl apply -f ../procurement-supply-optimization/kafka.yaml -n procurement-supply-optimization
+kubectl apply -f ../procurement-supply-optimization/suppliers.yaml -n procurement-supply-optimization
+kubectl apply -f ../procurement-supply-optimization/purchase-plans.yaml -n procurement-supply-optimization
+kubectl apply -f ../procurement-supply-optimization/contracts.yaml -n procurement-supply-optimization
 
 # Commerce Sales
-kubectl apply -f ../commerce-sales/sales.yaml
+kubectl apply -f ../commerce-sales/sales.yaml -n commerce-sales
 
 # Financial Billing
-kubectl apply -f ../financial-billing/purchases.yaml
 
+kubectl apply -f ../financial-billing/purchases.yaml -n financial-billing
 # Wait for services to be ready
 echo "⏳ Waiting for services to be ready..."
-kubectl wait --for=condition=available --timeout=120s deployment/purchase-plan-service-v1 -n medisupply-eventing
-kubectl wait --for=condition=available --timeout=120s deployment/supplier-service-v1 -n medisupply-eventing
-kubectl wait --for=condition=available --timeout=120s deployment/sales-v1 -n medisupply-eventing
-kubectl wait --for=condition=available --timeout=120s deployment/purchases-v1 -n medisupply-eventing
+kubectl wait --for=condition=available --timeout=120s deployment/purchase-plan-service -n medisupply-eventing
+kubectl wait --for=condition=available --timeout=120s deployment/supplier-service -n medisupply-eventing
+kubectl wait --for=condition=available --timeout=120s deployment/sales -n medisupply-eventing
+kubectl wait --for=condition=available --timeout=120s deployment/purchases -n medisupply-eventing
 
 # Create triggers
 echo "🎯 Creating triggers..."
